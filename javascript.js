923f4caa72b3e1303d7a1fe5c2e9d4344aa38e7d
anand kumar <anand00kumar20@gmail.com>
2023-03-16 18:55:44 +0530

    function work start

---
+++ javascript.js
@@ -0,0 +1,13 @@
+console.log('anand')
+
+function boynow(){
+  console.log('click now function running')
+document.write('thank you for buying t shirt') 
+
+
+}
+console.log('boynow')
+document.querySelector('#boy-now').addEventListener('click', function(){
+  boynow()
+})
+
